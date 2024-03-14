9fab7e29cd5cce1ef7f0746baf63396a0969329b
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2024-03-14 22:14:16 +0700

    feat: add working api route with docker compose

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
+	"os"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hi"))
+		w.Write([]byte(os.Getenv("APP_NAME")))
 	})
 	http.ListenAndServe(":8000", r)
 
